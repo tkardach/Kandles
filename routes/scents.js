1cbcf93e77492293960eba320135cee3dfc78d7b
Kardach, Tommy <thomas.kardach@thermofisher.com>
2019-10-24 15:03:07 -0700

    Added in-line comments to scents route page

--- routes/scents.js
+++ routes/scents.js
@@ -8,23 +8,35 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
+// Get all scents in the database
+//  Must be an admin, we don't want random users
+// seeing ingredient items
 router.get('/', [auth, admin], async (req, res) => {
+  // get all scents in the database
   const scents = await Scent.find();
   res.status(200).send(scents);
 });
 
+// Get the scent by the given id
+//  Must be an admin, we don't want random users
+// seeing ingredient items
 router.get('/:id', [auth, admin, validateObjectId], async (req, res) => {
+  // find the scent with the given id
   const scent = await Scent.findById(req.params.id);
   if (!scent) return res.status(404).send('Could not find scent with matching Id.');
 
   res.status(200).send(scent);
 });
 
+// Post the scent with the given parameters
+//  Must be an admin, we don't want random users
+// creating scents
 router.post('/', [auth, admin], async (req, res) => {
-  // Validate the scent attributes
+  // validate the scent attributes
   const { error } = validatePostScent(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // create a scent with the requested parameters
   const scent = new Scent(_.pick(req.body,
     ['name',
       'soapSafe',
@@ -37,14 +49,19 @@ router.post('/', [auth, admin], async (req, res) => {
 
   await scent.save();
 
+  // return created scent
   res.status(200).send(scent);
 });
 
+// Put (Update) existing scent with given parameters
+//  Must be an admin, we don't want random users
+// altering existing scents
 router.put('/:id', [auth, admin, validateObjectId], async (req, res) => {
-  // Validate the scent attributes
+  // validate the scent attributes
   const { error } = validatePostScent(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // find and update the scent by id
   const scent = await Scent.findByIdAndUpdate(req.params.id, {
     $set: _.pick(req.body,
       ['name',
@@ -64,13 +81,19 @@ router.put('/:id', [auth, admin, validateObjectId], async (req, res) => {
     message: `Scent Updated: ${scent}`
   });
 
+  // return updated scent
   res.status(200).send(scent);
 });
 
+// Delete the scent with the requested Id
+//  Must be an admin, we don't want random users
+// deleting scents
 router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
+  // find and delete scent by id
   const scent = await Scent.findByIdAndDelete(req.params.id);
   if (!scent) return res.status(404).send('Scent with that id not found');
 
+  // return deleted scent
   res.status(200).send(scent);
 });
 
